283bae3b6231221b75fa55910975f4d858699283
Ahmadkashif <ahmadkashif983@gmail.com>
2021-03-09 15:44:53 +0500

    init commit

--- src/main/java/StringSplitter.java
+++ src/main/java/StringSplitter.java
@@ -3,7 +3,6 @@ public class StringSplitter {
     public static String[] solution(String s){
 
         String[] strArr;
-
         if(s.length()>0){
 
             int strLen = ( ((s.length()% 2) == 0) ? s.length()/2 : (s.length()/2) + 1  ) ;
