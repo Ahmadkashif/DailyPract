14e5cdf56806d33a090b54ae373ba0a1618cea40
Ahmadkashif <ahmadkashif983@gmail.com>
2021-03-15 10:14:58 +0500

    bubble sort

--- src/main/java/Algorithms/BubbleSort.java
+++ src/main/java/Algorithms/BubbleSort.java
@@ -4,18 +4,22 @@ public class BubbleSort {
 
     public static int[] sort(int[] arr){
 
+        int unsortedIndex = arr.length - 1;
+
         for(int i = 0; i < arr.length ;i++){
+            for(int j= 0 ; j < unsortedIndex; j++) {
 
-            if((i+1) < arr.length) {
+                if((i+1) < arr.length) {
 
-                if(arr[i] > arr[ i+1 ]){
-                    int temp = arr[i+1];
-                    arr[i+1] = arr[i];
-                    arr[i] = temp;
+                    if(arr[i] > arr[ i+1 ]){
+                        int temp = arr[i+1];
+                        arr[i+1] = arr[i];
+                        arr[i] = temp;
+                    }
                 }
-            }
-            else{
-                continue;
+                else{
+                    continue;
+                }
             }
         }
         return arr;
