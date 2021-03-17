295546f2861eaa9fd5b2da7a2fd9a1e6c072a3e8
Ahmadkashif <ahmadkashif983@gmail.com>
2021-03-17 17:53:45 +0500

    ArrayList

---
+++ src/main/java/DataStructures/ARRAYList.java
@@ -0,0 +1,14 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class ARRAYList {
+
+    public ARRAYList(Object data) {
+
+    }
+
+    private Object String;
+    private ArrayList<Object> ArrLst = new ArrayList<Object>();
+    
+}
