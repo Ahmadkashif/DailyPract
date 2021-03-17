2401aeb6ca0e1332ae6df73e0cd5e03f0c86d4de
Ahmadkashif <ahmadkashif983@gmail.com>
2021-03-17 21:00:38 +0500

    Generics

--- src/main/java/GeneralPractice/GenericsPractice/Team.java
+++ src/main/java/GeneralPractice/GenericsPractice/Team.java
@@ -1,4 +1,9 @@
 package GeneralPractice.GenericsPractice;
 
 public class Team {
+
+    private int matchesPlayed = 0;
+    private int matchesWon = 0;
+    private int matchesDrawn = 0;
+    private int
 }
